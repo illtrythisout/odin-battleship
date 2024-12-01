f3322b67b5c8c85fec4ac0984e45fc6449462d3e
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-11-30 23:44:44 -0300

    Feat: Create placeShipsRandomly method in Player

--- src/player.js
+++ src/player.js
@@ -5,4 +5,24 @@ export default class Player {
     this.type = type;
     this.board = new Gameboard();
   }
+
+  placeShipsRandomly() {
+    const shipLengths = [2, 3, 3, 4, 5];
+
+    shipLengths.forEach((length) => {
+      let placed = false;
+
+      while (!placed) {
+        const row = Math.random() * 10;
+        const col = Math.random() * 10;
+        const direction = Math.random() > 0.5 ? 'horizontal' : 'vertical';
+
+        // if it is possible to place the ship, place it
+        if (this.board.placeShip(length, row, col, direction)) {
+          this.board.placeShip(length, row, col, direction);
+          placed = true;
+        }
+      }
+    });
+  }
 }
