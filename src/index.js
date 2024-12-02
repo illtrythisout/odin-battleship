9da8cd92f35b0847b8f10328317e491b763561e0
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-12-01 23:37:42 -0300

    Feat: Create runGame func()
    - Simulates running the game in the console using console.logs and prompts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './styles.css';
+import Gameboard from './gameboard';
+import Player from './player';
+
+function runGame() {
+  let player = new Player('human');
+  let computer = new Player('computer');
+  computer.placeShipsRandomly();
+  player.placeShipsRandomly();
+
+  // while no one has won
+  while (!player.board.areAllShipsSunk() && !computer.board.areAllShipsSunk()) {
+    // player attacks computer
+    let row = parseInt(prompt('row')); // REPLACE WITH DOM INPUT
+    let col = parseInt(prompt('col')); // REPLACE WITH DOM INPUT
+
+    if (!computer.board.receiveAttack(row, col)) {
+      break;
+    }
+
+    console.log('COMPUTER BOARD:'); // REPLACE WITH DOM OUTPUT
+    computer.board.board.forEach((row) => console.log(row));
+
+    player.getShotRandomly();
+    console.log('PLAYER BOARD:'); // REPLACE WITH DOM OUTPUT
+    player.board.board.forEach((row) => console.log(row));
+  }
+}
